73d2031975b2667d61371cae75546039f8eab38a
danilowlc <danilowlc@gmail.com>
2023-06-21 05:35:09 -0300

    configs inicial

---
+++ src/styles/index.ts
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from 'styled-components'
+
+const EstiloGlocal = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: Roboto, sans-serif;
+  }
+`
+
+export default EstiloGlocal
