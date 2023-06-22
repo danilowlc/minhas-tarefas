8add6832d10e10b74b139ed7f95c11bcac5f5404
danilowlc <danilowlc@gmail.com>
2023-06-22 06:40:56 -0300

    criando Cards

--- src/containers/ListaDeTarefas/index.tsx
+++ src/containers/ListaDeTarefas/index.tsx
@@ -1,7 +1,43 @@
+import Tarefa from '../../components/Tarefa'
+import { Container } from './styles'
+
+const tarefas = [
+  {
+    titulo: 'Estudar TypeScript',
+    decrisao: 'Assistir a aula 3 da EBAC',
+    prioridade: 'importante',
+    status: 'pendente'
+  },
+  {
+    titulo: 'Pagar COSERN',
+    decrisao: 'Pagar COSERN ate o dia 23/06',
+    prioridade: 'urgente',
+    status: 'concluida'
+  },
+  {
+    titulo: 'Estudar Python',
+    decrisao: 'Assistir a aula 3 da Pythonando',
+    prioridade: 'importante',
+    status: 'pendente'
+  }
+]
+
 const ListaDeTarefas = () => (
-  <main>
+  <Container>
     <p>2 tarefas marcadas como: &quot;categoria&ldquo; e &quot;termo&ldquo;</p>
-  </main>
+    <ul>
+      {tarefas.map((t) => (
+        <li key={t.titulo}>
+          <Tarefa
+            descricao={t.decrisao}
+            titulo={t.titulo}
+            prioridade={t.prioridade}
+            status={t.status}
+          />
+        </li>
+      ))}
+    </ul>
+  </Container>
 )
 
 export default ListaDeTarefas
